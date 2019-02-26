393096650cb291f48f4fd91f8c474becff8108fe
Colinice123 <2779159717@qq.com>
2019-02-26 13:17:23 +0800

    张三第一次修改

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -5,12 +5,7 @@ public class Main {
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
-		System.out.println("HelloWorld1!");
-		System.out.println("HelloWorld1!ÀîËÄ");
-<<<<<<< HEAD
-		System.out.println("HelloWorld123");
-=======
-		System.out.println("HelloWorldzhangsan");
->>>>>>> parent of 25266c0... Merge branch 'master' of github.com:Colinice123/mytest
+	    System.out.println("HelloWorld!zhangsan1");
+
     }
 }
